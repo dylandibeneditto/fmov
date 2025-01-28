d401e51acef46a224cae694dcec9ab3cae82d450
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-01-27 23:49:43 -0500

    feat: code structure outline in tests

---
+++ tests/test_core.py
@@ -0,0 +1,27 @@
+import "fmov"
+from PIL import Image, ImageDraw
+
+# create video with pipe
+# runs in parellel with the image rendering
+video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8)
+
+for i in range(10):
+
+    image = Image.new("RGB", (1920, 1080), "#ffffff")
+    draw = ImageDraw.Draw(image)
+
+    draw.text((100+i,100), "hello world", fill="#000000")
+
+    video.sound_at_frame(frame=i, path="./mysound.wav")
+    video.pipe(image)
+
+video.sound_at_time(time=1000, path="./mysound.wav")
+
+video.save()
+
+# create video through collecting frames from disk
+video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8)
+
+video.from_images(path="./out/*.png")
+
+video.save()
