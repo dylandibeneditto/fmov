4b325423b5c7f92efa2f8055d7e4fd58560acf1a
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-01-28 21:12:39 -0500

    example for testing, minute long dvd bounce effect

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,14 +2,24 @@ import fmov
 from PIL import Image, ImageDraw
 
 # create video with pipe
-video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
+video = fmov.Video((1920,1080), framerate=60, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
 
-for i in range(100):
+x,y = (0,0)
+vx, vy = (2,2)
+for i in range(60*60):
 
     image = Image.new("RGB", (1920, 1080), "#ffffff")
     draw = ImageDraw.Draw(image)
 
-    draw.text((100+i,100), "hello world", fill="#000000")
+    draw.rectangle((x,y,x+50,y+50), fill="#000000")
+
+    if x+vx >= 1920-50 or x+vx <= 0:
+        vx *= -1
+    if y+vy >= 1080-50 or y+vy <= 0:
+        vy *= -1
+
+    x += vx
+    y += vy
 
     #video.sound_at_frame(frame=i, path="./mysound.wav")
     video.pipe(image)
