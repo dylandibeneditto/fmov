b26bf10ada89f670b4faef0eccc0b8b3a0d5438d
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-01-28 20:16:52 -0500

    simple initial implementation

--- fmov/video.py
+++ fmov/video.py
@@ -1,4 +1,9 @@
-from typing import Tuple
+from typing import Tuple, Any
+import numpy
+import ffmpeg
+from numpy.typing import NDArray
+from fmov.audio import Audio
+from PIL.Image import Image
 
 class Video:
     def __init__(
@@ -20,5 +25,47 @@ class Video:
         self.render_preset = render_preset
         self.crf = crf
         self.audio_bitrate = audio_bitrate
-        self.__audio_stamps: list[Audio] = []
-        self.__frames: list[NDArray[Any]] = []
+        #self.__audio_stamps: list[Audio] = []
+        self.__frames: list[Image] = []
+        self.__quick_image_multiplier = 2
+        self.__quick_frame_multiplier = 2
+
+    def pipe(self, image: Image):
+        """
+        adds a PIL image to the render stack
+        """
+        self.__frames.append(image)
+
+    def render(self, quick_image: bool = False, quick_frames: bool = False):
+        image_multiplier = self.__quick_image_multiplier if quick_image else 1
+        frame_multiplier = self.__quick_frame_multiplier if quick_frames else 1
+        process = ffmpeg.input(
+            "pipe:",
+            format="rawvideo",
+            pix_fmt="rgb24",
+            s=f'{self.dimensions[0]//image_multiplier}x{self.dimensions[1]//image_multiplier}',
+            framerate=self.framerate//frame_multiplier
+        ).output(
+            self.path,
+            vcodec=self.vcodec,
+            pix_fmt=self.pix_fmt,
+            preset=self.render_preset,
+            loglevel="quiet",
+            framerate=self.framerate//frame_multiplier,
+            crf=self.crf
+        ).overwrite_output().run_async(pipe_stdin=True)
+        
+        for (i, frame) in enumerate(self.__frames):
+            if i % frame_multiplier == 0:
+                final_frame = frame
+
+                if quick_image:
+                    (w, h) = frame.size
+                    final_frame.resize((w//self.__quick_image_multiplier, h//self.__quick_image_multiplier))
+
+                # convert image to bytes and pipe to ffmpeg
+                process.stdin.write(numpy.array(final_frame).tobytes)
+        
+        # end the pipe process
+        process.stdin.close()
+        process.wait()
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,4 @@
-import fmov
+import fmov.fmov
 from PIL import Image, ImageDraw
 
 # create video with pipe
@@ -12,16 +12,16 @@ for i in range(10):
 
     draw.text((100+i,100), "hello world", fill="#000000")
 
-    video.sound_at_frame(frame=i, path="./mysound.wav")
+    #video.sound_at_frame(frame=i, path="./mysound.wav")
     video.pipe(image)
 
-video.sound_at_time(time=1000, path="./mysound.wav")
+#video.sound_at_time(time=1000, path="./mysound.wav")
 
-video.save()
+video.render()
 
 # create video through collecting frames from disk
-video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
+#video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
 
-video.from_images(path="./out/*.png")
+#video.from_images(path="./out/*.png")
 
-video.save()
+#video.save()
