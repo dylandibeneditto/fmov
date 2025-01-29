1f8f70461cf199066e79048ccef0180bdf0addff
Dylan DiBeneditto <dibenedittod@gmail.com>
2025-01-29 03:08:48 -0500

    finished dvd bounce effect for testing and examples

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,36 +1,62 @@
 import fmov
 from PIL import Image, ImageDraw
+import random
+from rich.progress import track
 
 # create video with pipe
-video = fmov.Video((1920,1080), framerate=60, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
+video = fmov.Video((1920,1080), framerate=60, path="./video.mp4")
 
+# position and velocity of the dvd logo
 x,y = (0,0)
-vx, vy = (2,2)
-for i in range(60*60):
+vx, vy = (video.framerate//2,video.framerate//2)
 
-    image = Image.new("RGB", (1920, 1080), "#ffffff")
+# create dvd image as a PIL image
+dvd_img = Image.open("./tests/dvd-logo.png")
+
+# values to resize the dvd image for the video
+img_height = 150
+aspect_ratio = dvd_img.width / dvd_img.height
+img_width = int(img_height * aspect_ratio)
+dvd_img = dvd_img.resize((img_width, img_height))
+
+# the hue shift value of the dvd logo
+hue = 0
+
+# the frame index 4 minutes into the video
+total_frames = video.minutes_to_frame(4)
+
+for i in track(range(total_frames), "Rendering...", total=total_frames):
+    # initializing PIL variables
+    image = Image.new("RGB", (video.width, video.height), "#000000")
     draw = ImageDraw.Draw(image)
 
-    draw.rectangle((x,y,x+50,y+50), fill="#000000")
+    # adding the dvd image
+    # finding the fill color based on the hue, turn it into an image, and use the dvd image as a mask
+    fill_color = Image.new("HSV", (1, 1), (hue, 200, 220)).convert("RGB").getpixel((0, 0))
+    color_layer = Image.new("RGB", dvd_img.size, fill_color)
+    image.paste(color_layer, (x, y), dvd_img.convert("L") if dvd_img.mode != "RGBA" else dvd_img.split()[3])
 
-    if x+vx >= 1920-50 or x+vx <= 0:
+    # collision detection
+    bumped = False
+    if x+vx >= video.width-img_width or x+vx <= 0:
         vx *= -1
-    if y+vy >= 1080-50 or y+vy <= 0:
+        bumped = True
+    if y+vy >= video.height-img_height or y+vy <= 0:
         vy *= -1
+        bumped = True
 
+    if bumped:
+        hue = (hue+random.randint(20,60))%255
+
+    # position updates
     x += vx
     y += vy
 
     #video.sound_at_frame(frame=i, path="./mysound.wav")
+    # append the frame to the video
     video.pipe(image)
 
 #video.sound_at_time(time=1000, path="./mysound.wav")
 
+# render the video 🥳
 video.render()
-
-# create video through collecting frames from disk
-#video = fmov.Video((1920,1080), framerate=30, path="./video.mp4", vcodec="libx264", pix_fmt="yuv420p", render_preset="ultrafast", crf=8, audio_bitrate="192k")
-
-#video.from_images(path="./out/*.png")
-
-#video.save()
