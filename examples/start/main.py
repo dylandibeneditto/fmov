2c1904874037612f08ef05d4e30115ee8711daf9
dylan <dibenedittod@gmail.com>
2026-02-15 22:34:40 -0500

    feat: added examples

--- examples/start/main.py
+++ examples/start/main.py
@@ -4,7 +4,7 @@ from tqdm import tqdm
 
 # using 'with Video() as video' makes rendering simpler, as when the context ends it calls render automatically
 with Video(path="video.mp4", width=1920, height=1080, fps=60) as video:
-    total_frames = video.time_to_frame("10s") # turns the timestamp "60s" into the number of frames in the video
+    total_frames = video.time_to_frame("20s") # turns the timestamp "20s" into the number of frames in the video
 
     # create all the frames in the video (tqdm is a progress bar)
     for i in tqdm(range(total_frames), total=total_frames, desc="Rendering"):
